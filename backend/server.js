bdd69827528b9e42a07e54c8e1cc2732b0709b19
obaraelijah <elijahobara357@gmail.com>
2023-08-25 16:48:05 +0300

    chore: setup basic express server

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import chalk from "chalk";
+import path from "path";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import mongosanitize from "express-mongo-sanitize"
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(mongosanitize());
+
+app.get("/api/v1/test", (req, res) => {
+    res.json({ Hi: "Welcome to the Invoice App" });
+});
+
+
+const PORT = process.env.PORT || 1997;
+
+app.listen(PORT, () => {
+    console.log(
+        `${chalk.green.bold("✔")} 👍 Server running in ${chalk.yellow.bold(
+            process.env.NODE_ENV
+        )} mode on port ${chalk.blue.bold(PORT)}`
+    );
+});
