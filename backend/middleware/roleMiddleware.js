b3b9634aaf845d22909a0fbd29441f01d58092f5
obaraelijah <elijahobara357@gmail.com>
2023-08-28 15:12:37 +0300

    feat: add role,error,rate limit, auth middleware

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,12 +1,33 @@
 import { ADMIN, USER } from "../constants/index.js";
 
 const ROLES = {
-    User: USER,
-    Admin: ADMIN,
+	User: USER,
+	Admin: ADMIN,
 };
 
+// role-based authorization 
 const checkRole = (...allowedRoles) => {
-    return (req, res, next) => {
-        
-    }
+	return (req, res, next) => {
+		if (!req?.user && !req?.roles) {
+			res.status(401);
+			throw new Error("You are not authorized to use our platform");
+		}
+
+		const rolesArray = [...allowedRoles];
+
+		const roleFound = req.roles
+			.map((role) => rolesArray.includes(role))
+			.find((value) => value === true);
+
+		if (!roleFound) {
+			res.status(401);
+			throw new Error("You are not authorized to perform this request");
+		}
+
+		next();
+	};
 };
+
+const role = { ROLES, checkRole };
+
+export default role;
