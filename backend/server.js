9bd5528cb9f6b83aef8d4f63471f935f273665a6
obaraelijah <elijahobara357@gmail.com>
2023-09-05 19:39:29 +0300

    feat: add routes to server and fix code quality

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,31 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
-import mongosanitize from "express-mongo-sanitize"
-import connectionToDB from "./config/connectDB.js";
 import morgan from "morgan";
+import connectionToDB from "./config/connectDB.js";
 import { morganMiddleware, systemLogs } from "./utils/Logger.js";
+import mongoSanitize from "express-mongo-sanitize";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { apiLimiter } from "./middleware/apiLimiter.js";
+import passport from "passport";
+import googleAuth from "./config/passportSetup.js";
+import customerRoutes from "./routes/customerRoutes.js";
+import documentRoutes from "./routes/documentRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 await connectionToDB();
 
 const app = express();
+
+const __dirname = path.resolve(); // Get the current directory's absolute path
+
+// Serve files from the "/uploads" directory at the "/uploads" route
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// Serve files from the "/docs" directory at the "/docs" route
+app.use("/docs", express.static(path.join(__dirname, "/docs")));
 
 
 if (process.env.NODE_ENV === "development") {
@@ -23,6 +38,9 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+app.use(passport.initialize());
+googleAuth();
+
 app.use(cookieParser());
 
 app.use(mongosanitize());
@@ -32,6 +50,12 @@ app.use(morganMiddleware);
 app.get("/api/v1/test", (req, res) => {
     res.json({ Hi: "Welcome to the Invoice App" });
 });
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", apiLimiter, userRoutes);
+app.use("/api/v1/customer", apiLimiter, customerRoutes);
+app.use("/api/v1/document", apiLimiter, documentRoutes);
+app.use("/api/v1/upload", apiLimiter, uploadRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
